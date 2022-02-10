cd7136fe2b4d7cc3f489173783ce5bbd059487df
Junyong Park <jyp0802@hotmail.com>
2022-02-10 14:39:01 +0900

    Minor updates

--- install_local.sh
+++ install_local.sh
@@ -20,5 +20,4 @@ echo "import os; DATA_DIR=os.path.abspath('.')" >> ../human_aware_rl/human_aware
 cp -r server/static app/
 cp -r server/*.py app/
 cp -r server/graphics/overcooked_graphics_v2.2.js app/static/js/graphics.js
-cp -r server/graphics/,my_config.json app/static/js/my_config.json
 cp -r server/config.json app/
